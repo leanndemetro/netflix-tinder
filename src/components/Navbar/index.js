8f8829def6959b0186d9735bbfb2157dafc5bbbe
Alyssa DeMetro <leanndemetro@gmail.com>
2021-08-06 19:10:11 -0400

    minor change

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,18 +12,6 @@ function Navbar() {
       <div>
         <ul className="navbar-nav">
           <li className="nav-item">
-            {/* <Link
-              to="/"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/about"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              About
-            </Link> */}
-          </li>
-          <li className="nav-item">
             <Link
               to="/discover"
               className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
@@ -31,14 +19,6 @@ function Navbar() {
               Discover
             </Link>
           </li>
-          {/* <li className="nav-item">
-            <Link
-              to="/search"
-              className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}
-            >
-              Search
-            </Link>
-          </li> */}
         </ul>
       </div>
     </nav>
