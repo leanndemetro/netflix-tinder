87eaa3efbb394db5e351175169455cd84703b3ba
Alyssa DeMetro <leanndemetro@gmail.com>
2021-08-14 18:48:17 -0400

    signup form css rough draft update

--- src/components/SignupForm/index.js
+++ src/components/SignupForm/index.js
@@ -4,14 +4,14 @@ import "./style.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function SignupForm() {
   return (
-    <div>
+    <div className="signupForm">
       <form action="/submit" method="post">
-    <h2>Create A User</h2>
-    <input type="text" class="form-control" name="firstName" placeholder="First Name" autofocus></input>
-    <input type="text" class="form-control" name="lastName" placeholder="Last Name" autofocus></input>
-    <input type="text" class="form-control" name="email" placeholder="Email" autofocus></input>
-    <input type="text" class="form-control" name="password" placeholder="Password" autofocus></input>
-    <button class="btn btn-lg btn-primary btn-block" id="submit" type="submit">Submit</button>
+    <h1 className="formLabel">Sign Up</h1>
+    <input type="text" className="form-control input" name="firstName" placeholder="First Name" autofocus></input>
+    <input type="text" className="form-control input" name="lastName" placeholder="Last Name" autofocus></input>
+    <input type="text" className="form-control input" name="email" placeholder="Email" autofocus></input>
+    <input type="text" className="form-control input" name="password" placeholder="Password" autofocus></input>
+    <button class="btn btn-lg btn-danger btn-block submitButton" id="submit" type="submit">Submit</button>
   </form>
     </div>
 
